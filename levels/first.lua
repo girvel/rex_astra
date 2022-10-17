ebafe75faced7e2ea81b70a8e112e60ff16454a6
girvel <widauka@ya.ru>
2022-10-17 11:49:19 +0400

    Add three more provinces

--- levels/first.lua
+++ levels/first.lua
@@ -1,5 +1,7 @@
 return {load = function(world)
-	local ai1 = world:addEntity {
+	local r = {}
+
+	r.ai1 = world:addEntity {
 		name = "AI_1",
 		color = standard.palette.ai1,
 		gold = 0,
@@ -9,7 +11,7 @@ return {load = function(world)
 
 	zandara:add_planet()
 
-	local sod = zandara:add_province {
+	r.sod = zandara:add_province {
 		name = "Coast of Sod",
 		garrison = 3,
 		anchor_position = vector {233, 35},
@@ -18,15 +20,15 @@ return {load = function(world)
 		maximal_garrison = 8,
 	}
 
-	local annar = zandara:add_province {
+	r.annar = zandara:add_province {
 		name = "Taiga of Annar",
 		garrison = 2,
 		anchor_position = vector {220, 66},
 		fertility = .05,
-		owner = ai1,
+		owner = r.ai1,
 	}
 
-	local dowur = zandara:add_province {
+	r.dowur = zandara:add_province {
 		name = "Dowur lowlands",
 		garrison = 2,
 		anchor_position = vector {248, 56},
@@ -34,67 +36,88 @@ return {load = function(world)
 		owner = player,
 	}
 
-	local venedai = zandara:add_province {
+	r.venedai = zandara:add_province {
 		name = "Venedai reach",
 		garrison = 2,
 		anchor_position = vector {189, 77},
 		fertility = .11,
-		owner = ai1,
+		owner = r.ai1,
 	}
 
-	local zanartha = zandara:add_province {
+	r.zanartha = zandara:add_province {
 		name = "Zanartha highland",
 		garrison = 2,
 		anchor_position = vector {236, 92},
 		fertility = .06,
-		owner = ai1,
+		owner = r.ai1,
 	}
 
-	local jadia = zandara:add_province {
+	r.jadia = zandara:add_province {
 		name = "Jadia edge",
 		fertility = .07,
 		anchor_position = vector {170, 90},
 	}
 
-	local reimin = zandara:add_province {
+	r.reimin = zandara:add_province {
 		name = "Reimin plains",
 		fertility = .10,
 		anchor_position = vector {220, 100},
 	}
 
-	local uxan = zandara:add_province {
+	r.uxan = zandara:add_province {
 		name = "Uxan peaks",
 		fertility = .02,
 		anchor_position = vector {259, 98},
 	}
 
-	local reidan = zandara:add_province {
+	r.reidan = zandara:add_province {
 		name = "Reidan",
 		fertility = .15,
 		anchor_position = vector {266, 66},
 	}
 
-	local lower_mikara = zandara:add_province {
+	r.lower_mikara = zandara:add_province {
 		name = "Lower mikara",
 		fertility = .01,
 		anchor_position = vector {186, 21},
 	}
 
-	local higher_mikara = zandara:add_province {
+	r.higher_mikara = zandara:add_province {
 		name = "Higher mikara",
 		fertility = .003,
 		anchor_position = vector {171, 21},
 	}
 
-	higher_mikara.neighbours = {lower_mikara}
-	lower_mikara.neighbours = {higher_mikara, sod}
-	sod.neighbours = {annar, dowur, lower_mikara}
-	annar.neighbours = {sod, dowur, venedai, zanartha, uxan}
-	dowur.neighbours = {sod, annar, uxan, reidan}
-	venedai.neighbours = {annar, jadia, reimin}
-	zanartha.neighbours = {annar, reimin, uxan}
-	jadia.neighbours = {venedai, reimin}
-	reimin.neighbours = {jadia, venedai, annar, zanartha, uxan}
-	uxan.neighbours = {reimin, zanartha, annar, dowur, reidan}
-	reidan.neighbours = {uxan, dowur}
+	r.antaris = zandara:add_province {
+		name = "Antaris archipelago",
+		fertility = .09,
+		anchor_position = vector {85, 30},
+	}
+
+	r.devarus = zandara:add_province {
+		name = "Devarus",
+		fertility = .13,
+		anchor_position = vector {75, 46},
+	}
+
+	r.fulthu = zandara:add_province {
+		name = "Lost peninsula of Fulthu",
+		fertility = .14,
+		anchor_position = vector {126, 73},
+	}
+
+	r.fulthu.neighbours = {r.devarus, r.jadia}
+	r.devarus.neighbours = {r.antaris, r.fulthu}
+	r.antaris.neighbours = {r.higher_mikara, r.devarus}
+	r.higher_mikara.neighbours = {r.lower_mikara, r.antaris}
+	r.lower_mikara.neighbours = {r.higher_mikara, r.sod}
+	r.sod.neighbours = {r.annar, r.dowur, r.lower_mikara}
+	r.annar.neighbours = {r.sod, r.dowur, r.venedai, r.zanartha, r.uxan}
+	r.dowur.neighbours = {r.sod, r.annar, r.uxan, r.reidan}
+	r.venedai.neighbours = {r.annar, r.jadia, r.reimin}
+	r.zanartha.neighbours = {r.annar, r.reimin, r.uxan}
+	r.jadia.neighbours = {r.venedai, r.reimin}
+	r.reimin.neighbours = {r.jadia, r.venedai, r.annar, r.zanartha, r.uxan}
+	r.uxan.neighbours = {r.reimin, r.zanartha, r.annar, r.dowur, r.reidan}
+	r.reidan.neighbours = {r.uxan, r.dowur}
 end}
